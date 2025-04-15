455db652f5085962cde8b091060f8e55fdb9c48f
Kai <dk786161616@gmail.com>
2025-04-15 19:47:08 +0530

    activity service done

--- activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
+++ activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
@@ -21,7 +21,7 @@ import java.util.Map;
 public class Activity {
     @Id
     private String id;
-    private String uerId;
+    private String userId;
     private ActivityType type;
     private Integer duration;
     private Integer caloriesBurned;
